78e00f7113c7fef3e17c85360e510d02a267b51f
dss-neto <192475519+dss-neto@users.noreply.github.com>
2025-03-09 17:13:50 -0300

    Add keyboard interaction

--- script.js
+++ script.js
@@ -2,37 +2,71 @@ const display = document.getElementById("display");
 const numberDiv = document.getElementById("numbers");
 const operatorsDiv = document.getElementById("operators");
 
+const NUMBERS_LIST = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+const OPERATORS_LIST = ["=", "+", "-", "*", "/"];
+
 let resultFromEqual = 0;
 let firstNumber = [0];
 let secondNumber = [];
 let currentOperating = firstNumber;
-/* showNext: if true, the number will show after triggering resultFromEqual with
-   an operator, instead of appending to the displayed number */
+/* showNext: if true, the number will show after triggering
+  resultFromEqual with an operator, instead of appending to the displayed number */
 let showNext = false;
 let operator;
 display.textContent = firstNumber;
+const keyboardObj = {
+  Backspace: function () {
+    backspaceFunctionality();
+  },
+  Enter: function () {
+    workWithOperators("=");
+  },
+  Escape: function () {
+    resetEverything();
+  },
+};
 
-numberDiv.addEventListener("click", (e) => workWithNumbers(e));
+document.addEventListener("keydown", (logKey) => {
+  if (
+    logKey.key !== "Enter" &&
+    logKey.key !== "Backspace" &&
+    logKey.key !== "Escape"
+  ) {
+    if (NUMBERS_LIST.includes(logKey.key)) {
+      workWithNumbers(logKey.key, "BUTTON");
+    } else if (OPERATORS_LIST.includes(logKey.key)) {
+      workWithOperators(logKey.key);
+    }
+  } else {
+    keyboardObj[logKey.key]();
+  }
+});
 
-operatorsDiv.addEventListener("click", (e) => workWithOperators(e));
+numberDiv.addEventListener("click", (e) =>
+  workWithNumbers(e.target.id, e.target.tagName)
+);
 
-function workWithNumbers(e) {
-  target = e.target;
-  if (target.id !== "." || !currentOperating.includes(".")) {
-    if (target.tagName === "BUTTON") {
+operatorsDiv.addEventListener("click", (e) => workWithOperators(e.target.id));
+
+function workWithNumbers(targetId, targetElement) {
+  if (targetId !== "." || !currentOperating.includes(".")) {
+    if (targetElement === "BUTTON") {
       if (currentOperating[0] === 0 && currentOperating.length === 1) {
         currentOperating.pop();
         /* Turn the initial 0 into nothing because the rest of the code 
            will display the number*/
         display.textContent = "";
       }
-      // If there is a resultFromEqual
       if (resultFromEqual) {
         resetEverything();
       }
-      currentOperating.push(target.textContent);
+      if (currentOperating.length === 1 && currentOperating[0] === 0) {
+        currentOperating.pop();
+        display.textContent = "";
+      }
+      currentOperating.push(targetId);
       if (!showNext) {
-        display.textContent += target.textContent;
+        display.textContent += targetId;
       } else {
         display.textContent = secondNumber;
         showNext = false;
@@ -40,60 +74,66 @@ function workWithNumbers(e) {
     }
   }
 }
-function workWithOperators(e) {
-  target = e.target;
-  if (target.tagName === "BUTTON") {
-    if (target.id !== "backspace") {
-      resultFromEqual = 0;
-      currentOperating = secondNumber;
-      showNext = true;
-    }
-    if (target.id === "AC") {
-      resetEverything();
-    } else if (target.id === "backspace") {
-      backspaceFunctionality();
-    } else if (target.id === "=") {
-      if (operator) {
-        resultFromEqual = firstNumber;
-        // This will make numbers negative if pressing equal
-        firstNumber.splice(
-          0,
-          firstNumber.length,
-          operate(
-            operator,
-            parseFloat(firstNumber.join("")),
-            parseFloat(secondNumber.join(""))
-          )
-        );
-        secondNumber = [];
-        display.textContent = firstNumber;
-        operator = null;
-      } else {
-        currentOperating = firstNumber;
-        showNext = false;
-      }
-      // If there is already an operator and someone presses it again
-    } else if (operator) {
-      if (secondNumber.length === 0) {
-        operator = target.id;
-      } else {
-        firstNumber.splice(
-          0,
-          firstNumber.length,
-          operate(
-            operator,
-            parseFloat(firstNumber.join("")),
-            parseFloat(secondNumber.join(""))
-          )
-        );
-        secondNumber.splice(0, secondNumber.length);
-        display.textContent = firstNumber;
-        operator = target.id;
-      }
-      // Reset all conditions if you press All Clear
+function workWithOperators(targetId) {
+  if (targetId !== "backspace") {
+    resultFromEqual = 0;
+    currentOperating = secondNumber;
+    showNext = true;
+  }
+  if (targetId === "AC") {
+    resetEverything();
+  } else if (targetId === "backspace") {
+    backspaceFunctionality();
+  } else if (targetId === "=") {
+    equalsFunctionality();
+  } else {
+    operatorFunctionality(targetId);
+  }
+}
+
+function operatorFunctionality(targetId) {
+  if (operator) {
+    if (secondNumber.length === 0) {
+      operator = targetId;
     } else {
-      operator = target.id;
+      firstNumber.splice(
+        0,
+        firstNumber.length,
+        operate(
+          operator,
+          parseFloat(firstNumber.join("")),
+          parseFloat(secondNumber.join(""))
+        )
+      );
       secondNumber.splice(0, secondNumber.length);
+      display.textContent = firstNumber;
+      operator = targetId;
+    }
+  } else {
+    operator = targetId;
+    secondNumber.splice(0, secondNumber.length);
+  }
+}
+
+function equalsFunctionality() {
+  if (secondNumber.length > 0) {
+    if (operator) {
+      resultFromEqual = firstNumber;
+      firstNumber.splice(
+        0,
+        firstNumber.length,
+        operate(
+          operator,
+          parseFloat(firstNumber.join("")),
+          parseFloat(secondNumber.join(""))
+        )
+      );
+      secondNumber = [];
+      display.textContent = firstNumber;
+      operator = null;
+    } else {
+      currentOperating = firstNumber;
+      showNext = false;
     }
   }
 }
@@ -107,6 +147,9 @@ function backspaceFunctionality() {
     currentOperating.splice(0, currentOperating.length, 0);
   }
   display.textContent = currentOperating.join("");
+  if (currentOperating.length === 0) {
+    display.textContent = 0;
+  }
 }
 
 function resetEverything() {
@@ -123,33 +166,22 @@ function resetEverything() {
 
 function operate(operator, num1, num2) {
   let operationObj = {
-    "+": add(num1, num2),
-    "-": subtract(num1, num2),
-    "×": multiply(num1, num2),
-    "÷": divide(num1, num2),
+    "+": function () {
+      return Number((num1 + num2).toFixed(7));
+    },
+    "-": function () {
+      return Number((num1 - num2).toFixed(7));
+    },
+    "/": function () {
+      if (num1 === 0 || num2 === 0) {
+        return "no way";
+      }
+      return Number((num1 / num2).toFixed(7));
+    },
+    "*": function () {
+      return Number((num1 * num2).toFixed(7));
+    },
   };
 
-  return operationObj[operator];
-}
-
-function add(num1, num2) {
-  // return arr.reduce((sum, number) => sum + number, 0);
-  return Number((num1 + num2).toFixed(7));
-}
-
-function subtract(num1, num2) {
-  // return arr.reduce((subtraction, number) => subtraction - number);
-  return Number((num1 - num2).toFixed(7));
-}
-
-function multiply(num1, num2) {
-  // return arr.reduce((total, number) => total * number);
-  return Number((num1 * num2).toFixed(7));
-}
-
-function divide(num1, num2) {
-  if (num1 === 0 || num2 === 0) {
-    return "no way";
-  }
-  return Number((num1 / num2).toFixed(7));
+  return operationObj[operator]();
 }
